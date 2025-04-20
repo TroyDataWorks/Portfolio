817bcd042f4f0fe82518bba5f792f739ec24b416
TroyDataWorks <tbohrer96@gmail.com>
2025-04-20 16:51:20 -0400

    project setup, tailwind is working.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+const App = () => {
+    return (
+        <main>
+            <div>
+                <h1 className='text-3xl'>Welcome!</h1>
+            </div>
+        </main>
+    )
+}
+
+export default App;
